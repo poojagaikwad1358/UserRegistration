7944459cb0dbfe24d23b49a593b82f2f05ee22b0
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-08-12 19:54:00 +0530

    Added: Regex & test cases for first name.

--- src/main/java/com/bridgelab/UserRegistration.java
+++ src/main/java/com/bridgelab/UserRegistration.java
@@ -1,7 +1,9 @@
 package com.bridgelab;
 
 public class UserRegistration {
-    public static void main(String[] args) {
-
+    public boolean firstName (String firstName) {
+        return firstName.matches("^[A-Z]{1}[a-zA-Z]{2,}");
     }
 }
+
+
--- src/test/java/com/bridgelab/UserRegistrationTest.java
+++ src/test/java/com/bridgelab/UserRegistrationTest.java
@@ -1,5 +1,29 @@
 package com.bridgelab;
 
-public class UserRegistrationTest{
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
+public class UserRegistrationTest{
+    UserRegistration userRegistration = new UserRegistration();
+    boolean result;
+
+    //Test cases for first name
+    @Test
+    public void firstName_returnsTrue_ifFirstNameProper() {
+        result = userRegistration.firstName("Pooja");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void firstName_returnsFalse_ifNumberOfCharDoesNotMatch() {
+        result = userRegistration.firstName("Po");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void firstName_returnFalse_ifFirstCharNotUppercase(){
+        result = userRegistration.firstName("pooja");
+        Assert.assertFalse(result);
+    }
 }
